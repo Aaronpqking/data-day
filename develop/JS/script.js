593d7fd6aba28af7309e88840c090d19c23019d4
Corey Skidmore <skidmoreeco@gmail.com>
2022-08-29 19:02:04 -0400

    second commit

--- develop/JS/script.js
+++ develop/JS/script.js
@@ -1,23 +1,5 @@
-var i = 0;
-var images = [];
-var time = 3000;
+var cityEl = document.querySelector("City")
+var zipEl = document.querySelector("Zip")
 
-images[0] = 'concert-background.jpg'
-images[1] = 'concert-background2.jpg'
-images[2] = '1947558.jpg'
-
-
-function changeImg() {
-    document.slide.src = images[i];
-
-
-    if(i < images.length - 1) {
-        i++;
-    } else {
-        i = 0;
-    }
-
-    setTimeout("changeImg()", time);
-}
-
-window.onload = changeImg;
+function getCityInfo(e)
+e.preventDefault();
